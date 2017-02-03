1d024c552f7cb52de30e62de1bfb1853884dd845
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2017-02-02 23:45:26 -0500

    Add additional types

--- src/types.js
+++ src/types.js
@@ -4,17 +4,39 @@ function isObjWithKey (key) {
   }
 }
 
-exports.isBool = isObjWithKey('bool')
-exports.isMust = isObjWithKey('must')
-exports.isTerm = isObjWithKey('term')
+var typeMap = {
+  bool: 'bool',
+  exists: 'exists',
+  geo_distance: 'geoDistance',
+  must: 'must',
+  must_not: 'mustNot',
+  nested: 'nested',
+  query: 'query',
+  range: 'range',
+  regexp: 'regexp',
+  should: 'should',
+  term: 'term'
+}
+
+exports.isBool        = isObjWithKey('bool')
+exports.isExists      = isObjWithKey('exists')
+exports.isGeoDistance = isObjWithKey('geo_distance')
+exports.isMust        = isObjWithKey('must')
+exports.isMustNot     = isObjWithKey('must_not')
+exports.isNested      = isObjWithKey('nested')
+exports.isQuery       = isObjWithKey('query')
+exports.isRange       = isObjWithKey('range')
+exports.isRegexp      = isObjWithKey('regexp')
+exports.isShould      = isObjWithKey('should')
+exports.isTerm        = isObjWithKey('term')
 
 exports.getType = function getType (node) {
-  if (exports.isBool(node)) {
-    return 'bool'
-  } else if (exports.isMust(node)) {
-    return 'must'
-  } else if (exports.isTerm(node)) {
-    return 'term'
+  if (node != null) {
+    for (prop in node) {
+      if (node.hasOwnProperty(prop) && prop in typeMap) {
+        return typeMap[prop]
+      }
+    }
   }
 
   throw new Error('Unrecognized ElasticSearch query node')
--- test/types.test.js
+++ test/types.test.js
@@ -3,30 +3,40 @@ var subject = require('../src/types')
 
 test('types - getType', function(t) {
   t.equal(subject.getType({ bool: {} }), 'bool', 'identifies bool nodes')
+  t.equal(subject.getType({ exists: { field: 'foo' } }), 'exists', 'identifies exists nodes')
+  t.equal(subject.getType({ geo_distance: {} }), 'geoDistance', 'identifies geo_distance nodes')
   t.equal(subject.getType({ must: [] }), 'must', 'identifies must nodes')
+  t.equal(subject.getType({ must_not: [] }), 'mustNot', 'identifies must_not nodes')
+  t.equal(subject.getType({ nested: {} }), 'nested', 'identifies nested nodes')
+  t.equal(subject.getType({ query: {} }), 'query', 'identifies query nodes')
+  t.equal(subject.getType({ range: {} }), 'range', 'identifies range nodes')
+  t.equal(subject.getType({ regexp: {} }), 'regexp', 'identifies regexp nodes')
+  t.equal(subject.getType({ should: [] }), 'should', 'identifies should nodes')
   t.equal(subject.getType({ term: { foo: 'bar' } }), 'term', 'identifies term nodes')
   t.throws(subject.getType.bind(null), 'throws given no input')
   t.throws(subject.getType.bind(null, {}), 'throws given unknown input')
   t.end()
 })
 
-test('types - isBool', function (t) {
-  t.equal(subject.isBool({ bool: {} }), true, 'returns true for bool nodes')
-  t.equal(subject.isBool({}), false, 'returns false for non-bool nodes')
-  t.equal(subject.isBool(null), false, 'returns false for nulls')
-  t.end()
-})
+function testIdentifier (funcName, key) {
+  test('types - ' + funcName, function (t) {
+    var node = {}
+    node[key] = {}
+    t.equal(subject[funcName](node), true, 'returns true for ' + key + ' nodes')
+    t.equal(subject[funcName]({}), false, 'returns false for non-' + key + ' nodes')
+    t.equal(subject[funcName](null), false, 'returns false for nulls')
+    t.end()
+  })
+}
 
-test('types - isMust', function (t) {
-  t.equal(subject.isMust({ must: [] }), true, 'returns true for must nodes')
-  t.equal(subject.isMust({}), false, 'returns false for non-must nodes')
-  t.equal(subject.isMust(null), false, 'returns false for nulls')
-  t.end()
-})
-
-test('types - isTerm', function (t) {
-  t.equal(subject.isTerm({ term: { foo: 'bar' } }), true, 'returns true for term nodes')
-  t.equal(subject.isTerm({}), false, 'returns false for non-term nodes')
-  t.equal(subject.isTerm(null), false, 'returns false for nulls')
-  t.end()
-})
+testIdentifier('isBool', 'bool')
+testIdentifier('isExists', 'exists')
+testIdentifier('isGeoDistance', 'geo_distance')
+testIdentifier('isMust', 'must')
+testIdentifier('isMustNot', 'must_not')
+testIdentifier('isNested', 'nested')
+testIdentifier('isQuery', 'query')
+testIdentifier('isRange', 'range')
+testIdentifier('isRegexp', 'regexp')
+testIdentifier('isShould', 'should')
+testIdentifier('isTerm', 'term')
