bcbfb85f3073efbcfd1c08b7a8ee4015828c2f1a
Jim Fitzpatrick <fitzpatrick.jim@gmail.com>
2017-02-04 22:26:47 -0500

    NodePath: Implement manipulation functions

--- src/NodePath.js
+++ src/NodePath.js
@@ -1,5 +1,11 @@
 var t = require('./types')
 
+var manipMap = {
+  must: 'must',
+  should: 'should',
+  mustNot: 'must_not'
+}
+
 function NodePath (node, parent) {
   this.node = node
   this.parent = parent
@@ -20,6 +26,64 @@ NodePath.prototype.findParent = function findParent (fn) {
     parent = parent.parent
   }
   return parent
+}
+
+/**
+ * Insert given node after context node.
+ *
+ * @param {object} node Node to insert
+ */
+NodePath.prototype.insertAfter = function insertAfter (node) {
+  var container = this.parent && this.parent.node[manipMap[this.parent.type]]
+  if (!container) {
+    throw new Error('Method not supported by this node')
+  }
+  container.splice(container.indexOf(this.node) + 1, 0, node)
+}
+
+/**
+ * Insert given node before context node.
+ *
+ * @param {object} node Node to insert
+ */
+NodePath.prototype.insertBefore = function insertBefore (node) {
+  var container = this.parent && this.parent.node[manipMap[this.parent.type]]
+  if (!container) {
+    throw new Error('Method not supported by this node')
+  }
+  container.splice(container.indexOf(this.node), 0, node)
+}
+
+/**
+ * Remove node from query.
+ */
+NodePath.prototype.remove = function remove () {
+  this.replaceWithMany([])
+}
+
+/**
+ * Replace current node with a given node.
+ *
+ * @param {object} node Node to replace this node with
+ */
+NodePath.prototype.replaceWith = function replaceWith (node) {
+  this.replaceWithMany([node])
+}
+
+/**
+ * Replace current node with a given nodes.
+ *
+ * @param {object[]} node Node to replace this node with
+ */
+NodePath.prototype.replaceWithMany = function replaceWithMany (nodes) {
+  var container = this.parent && this.parent.node[manipMap[this.parent.type]]
+  if (!container) {
+    throw new Error('Method not supported by this node')
+  }
+  var args = [container.indexOf(this.node), 1].concat(nodes)
+  container.splice.apply(container, args)
+  this._removed = true
+  this.skip()
 }
 
 /**
--- test/NodePath.test.js
+++ test/NodePath.test.js
@@ -36,6 +36,209 @@ test('NodePath#findParent', function (t) {
   t.end()
 })
 
+test('NodePath#insertAfter', function (t) {
+  var query = {
+    query: {
+      bool: {
+        must: [{ term: { foo: 'foo' } }],
+        should: [{ term: { foo: 'foo' } }],
+        must_not: [{ term: { foo: 'foo' } }]
+      }
+    }
+  }
+  var mustPath = new NodePath({ must: query.query.bool.must }, null)
+  var mustTermPath = new NodePath(query.query.bool.must[0], mustPath)
+  var shouldPath = new NodePath({ should: query.query.bool.should }, null)
+  var shouldTermPath = new NodePath(query.query.bool.should[0], shouldPath)
+  var mustNotPath = new NodePath({ must_not: query.query.bool.must_not }, null)
+  var mustNotTermPath = new NodePath(query.query.bool.must_not[0], mustNotPath)
+
+  var newNode = { term: { bar: 'bar' } }
+
+  mustTermPath.insertAfter(newNode)
+  shouldTermPath.insertAfter(newNode)
+  mustNotTermPath.insertAfter(newNode)
+
+  function insertWithoutParent () {
+    queryPath.insertAfter(newNode)
+  }
+  function insertUnimplemented () {
+    boolPath.insertAfter(newNode)
+  }
+
+  t.equal(query.query.bool.must.indexOf(newNode), 1, 'inserts node after self in must array')
+  t.equal(query.query.bool.should.indexOf(newNode), 1, 'inserts node after self in should array')
+  t.equal(query.query.bool.must_not.indexOf(newNode), 1, 'inserts node after self in must_not array')
+  t.throws(insertWithoutParent, 'throws if parent is null')
+  t.throws(insertUnimplemented, 'throws if node remove is unimplemented')
+  t.end()
+})
+
+test('NodePath#insertBefore', function (t) {
+  var query = {
+    query: {
+      bool: {
+        must: [{ term: { foo: 'foo' } }],
+        should: [{ term: { foo: 'foo' } }],
+        must_not: [{ term: { foo: 'foo' } }]
+      }
+    }
+  }
+  var mustPath = new NodePath({ must: query.query.bool.must }, null)
+  var mustTermPath = new NodePath(query.query.bool.must[0], mustPath)
+  var shouldPath = new NodePath({ should: query.query.bool.should }, null)
+  var shouldTermPath = new NodePath(query.query.bool.should[0], shouldPath)
+  var mustNotPath = new NodePath({ must_not: query.query.bool.must_not }, null)
+  var mustNotTermPath = new NodePath(query.query.bool.must_not[0], mustNotPath)
+
+  var newNode = { term: { bar: 'bar' } }
+
+  mustTermPath.insertBefore(newNode)
+  shouldTermPath.insertBefore(newNode)
+  mustNotTermPath.insertBefore(newNode)
+
+  function insertWithoutParent () {
+    queryPath.insertBefore(newNode)
+  }
+  function insertUnimplemented () {
+    boolPath.insertBefore(newNode)
+  }
+
+  t.equal(query.query.bool.must.indexOf(newNode), 0, 'inserts node before self in must array')
+  t.equal(query.query.bool.should.indexOf(newNode), 0, 'inserts node before self in should array')
+  t.equal(query.query.bool.must_not.indexOf(newNode), 0, 'inserts node before self in must_not array')
+  t.throws(insertWithoutParent, 'throws if parent is null')
+  t.throws(insertUnimplemented, 'throws if node remove is unimplemented')
+  t.end()
+})
+
+test('NodePath#remove', function (t) {
+  var query = {
+    query: {
+      bool: {
+        must: [{ term: { foo: 'foo' } }],
+        should: [{ term: { bar: 'bar' } }],
+        must_not: [{ term: { baz: 'baz' } }],
+      }
+    }
+  }
+
+  var queryPath = new NodePath(query, null)
+  var boolPath = new NodePath(query.query, queryPath)
+  var mustPath = new NodePath({ must: query.query.bool.must }, boolPath)
+  var mustTermPath = new NodePath(query.query.bool.must[0], mustPath)
+  var shouldPath = new NodePath({ should: query.query.bool.should }, boolPath)
+  var shouldTermPath = new NodePath(query.query.bool.should[0], shouldPath)
+  var mustNotPath = new NodePath({ must_not: query.query.bool.must_not }, boolPath)
+  var mustNotTermPath = new NodePath(query.query.bool.must_not[0], mustNotPath)
+
+  mustTermPath.remove()
+  shouldTermPath.remove()
+  mustNotTermPath.remove()
+
+  function removeWithoutParent () {
+    queryPath.remove()
+  }
+  function removeUnimplemented () {
+    boolPath.remove()
+  }
+
+  t.deepEqual(query.query.bool.must, [], 'removes node from must array')
+  t.deepEqual(query.query.bool.should, [], 'removes node from should array')
+  t.deepEqual(query.query.bool.must_not, [], 'removes node from must_not array')
+  t.ok(mustTermPath._removed, 'sets internal removed flag')
+  t.ok(mustTermPath._skip, 'sets internal skip flag')
+  t.throws(removeWithoutParent, 'throws if parent is null')
+  t.throws(removeUnimplemented, 'throws if node remove is unimplemented')
+  t.end()
+})
+
+test('NodePath#replaceWith', function (t) {
+  var query = {
+    query: {
+      bool: {
+        must: [{ term: { foo: 'foo' } }],
+        should: [{ term: { bar: 'bar' } }],
+        must_not: [{ term: { baz: 'baz' } }],
+      }
+    }
+  }
+
+  var queryPath = new NodePath(query, null)
+  var boolPath = new NodePath(query.query, queryPath)
+  var mustPath = new NodePath({ must: query.query.bool.must }, boolPath)
+  var mustTermPath = new NodePath(query.query.bool.must[0], mustPath)
+  var shouldPath = new NodePath({ should: query.query.bool.should }, boolPath)
+  var shouldTermPath = new NodePath(query.query.bool.should[0], shouldPath)
+  var mustNotPath = new NodePath({ must_not: query.query.bool.must_not }, boolPath)
+  var mustNotTermPath = new NodePath(query.query.bool.must_not[0], mustNotPath)
+
+  var newNode = { exists: { field: 'qux' } }
+
+  mustTermPath.replaceWith(newNode)
+  shouldTermPath.replaceWith(newNode)
+  mustNotTermPath.replaceWith(newNode)
+
+  function replaceWithoutParent () {
+    queryPath.remove()
+  }
+  function replaceUnimplemented () {
+    boolPath.remove()
+  }
+
+  t.deepEqual(query.query.bool.must, [newNode], 'replaces node in must array')
+  t.deepEqual(query.query.bool.should, [newNode], 'replaces node in should array')
+  t.deepEqual(query.query.bool.must_not, [newNode], 'replaces node in must_not array')
+  t.ok(mustTermPath._removed, 'sets internal removed flag')
+  t.ok(mustTermPath._skip, 'sets internal skip flag')
+  t.throws(replaceWithoutParent, 'throws if parent is null')
+  t.throws(replaceUnimplemented, 'throws if node remove is unimplemented')
+  t.end()
+})
+
+test('NodePath#replaceWithMany', function (t) {
+  var query = {
+    query: {
+      bool: {
+        must: [{ term: { foo: 'foo' } }],
+        should: [{ term: { bar: 'bar' } }],
+        must_not: [{ term: { baz: 'baz' } }],
+      }
+    }
+  }
+
+  var queryPath = new NodePath(query, null)
+  var boolPath = new NodePath(query.query, queryPath)
+  var mustPath = new NodePath({ must: query.query.bool.must }, boolPath)
+  var mustTermPath = new NodePath(query.query.bool.must[0], mustPath)
+  var shouldPath = new NodePath({ should: query.query.bool.should }, boolPath)
+  var shouldTermPath = new NodePath(query.query.bool.should[0], shouldPath)
+  var mustNotPath = new NodePath({ must_not: query.query.bool.must_not }, boolPath)
+  var mustNotTermPath = new NodePath(query.query.bool.must_not[0], mustNotPath)
+
+  var newNodes = [{ exists: { field: 'qux' } }, { exists: { field: 'norf' } }]
+
+  mustTermPath.replaceWithMany(newNodes)
+  shouldTermPath.replaceWithMany(newNodes)
+  mustNotTermPath.replaceWithMany(newNodes)
+
+  function replaceWithoutParent () {
+    queryPath.remove()
+  }
+  function replaceUnimplemented () {
+    boolPath.remove()
+  }
+
+  t.deepEqual(query.query.bool.must, newNodes, 'replaces node in must array')
+  t.deepEqual(query.query.bool.should, newNodes, 'replaces node in should array')
+  t.deepEqual(query.query.bool.must_not, newNodes, 'replaces node in must_not array')
+  t.ok(mustTermPath._removed, 'sets internal removed flag')
+  t.ok(mustTermPath._skip, 'sets internal skip flag')
+  t.throws(replaceWithoutParent, 'throws if parent is null')
+  t.throws(replaceUnimplemented, 'throws if node remove is unimplemented')
+  t.end()
+})
+
 test('NodePath#skip', function (t) {
   var query = { bool: { must: [{ term: { foo: 'bar' } }] } }
   var subject = new NodePath(query, null)
